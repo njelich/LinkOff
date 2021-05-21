076c252c29ad0fe51e63d22d824375c3558c295c
noa <git@jelich.cc>
2021-05-21 19:20:38 +0200

    Started work on account switching

--- background.js
+++ background.js
@@ -1,29 +1,54 @@
 // Set defaults on install.
 
-chrome.storage.local.get(null, function(res) {
-    if(res.initialized !== "v0.5") chrome.storage.local.set({
-        "initialized": "v0.5", 
-        "hide-account-building": false,
-        "hide-advertisements": true,
-        "hide-by-age": "week",
-        "hide-by-companies": true,
-        "hide-by-people": false,
-        "hide-commented-on": false,
-        "hide-follow-recommendations": true,
-        "hide-followed": true,
-        "hide-community-panel": true,
-        "hide-images": false,
-        "hide-linkedin-learning": true,
-        "hide-links": false,
-        "hide-polls": true,
-        "hide-premium": true,
-        "hide-promoted": true,
-        "hide-shared": false,
-        "hide-videos": false,
-        "hide-whole-feed": false,
-        "hide-liked": true,
-        "hide-other-reactions": false,
-        "main-toggle": true,
-        "sort-by-recent": true,
-    })
-  });
+chrome.storage.local.get(null, function (res) {
+  if (res.initialized !== "v0.5")
+    chrome.storage.local.set({
+      initialized: "v0.5",
+      "hide-account-building": false,
+      "hide-advertisements": true,
+      "hide-by-age": "week",
+      "hide-by-companies": true,
+      "hide-by-people": false,
+      "hide-commented-on": false,
+      "hide-follow-recommendations": true,
+      "hide-followed": true,
+      "hide-community-panel": true,
+      "hide-images": false,
+      "hide-linkedin-learning": true,
+      "hide-links": false,
+      "hide-polls": true,
+      "hide-premium": true,
+      "hide-promoted": true,
+      "hide-shared": false,
+      "hide-videos": false,
+      "hide-whole-feed": false,
+      "hide-liked": true,
+      "hide-other-reactions": false,
+      "main-toggle": true,
+      "sort-by-recent": true,
+    });
+});
+
+// Fetch login cookie
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+  if(request.name=="fetchLiAt") {
+    fetchLiAtCookie(request.data.accountList, request.data.name)
+  }
+  sendResponse("Got login cookie");
+});
+
+async function fetchLiAtCookie(accountList, name) {
+  chrome.cookies.getAll({ domain: "linkedin.com", name: "li_at" },cookie => {
+    console.log(cookie[0].value != accountList[name])
+    if (accountList[name] != cookie[0].value) {
+      const accounts = accountList;
+      accounts[name] = cookie[0].value
+      console.log(accounts)
+      chrome.storage.local.set({
+        "account-list": accounts,
+      });
+    }
+  })
+}
--- content/content.js
+++ content/content.js
@@ -2,7 +2,16 @@
 
 // Main function
 
-function doIt(res) {
+async function doIt(res) {
+  chrome.runtime.sendMessage(
+    { name: "fetchLiAt", data: {
+      accountList: res["account-list"], 
+      name: document.querySelector('img[class="global-nav__me-photo ember-view"]')?.alt,
+    }},
+    function (response) {
+        console.log(response);
+    }
+  )
   if(res['main-toggle']) {
     //Feed
     if(res['hide-whole-feed']) {
@@ -97,7 +106,7 @@ async function hideFeed() {
         }
         attempts = attempts + 1;
         resolve();
-      }, 100 * attempts * 10);
+      }, 100 + attempts * 10);
     });
   }
 }
@@ -117,7 +126,7 @@ async function showFeed() {
         }
         attempts = attempts + 1;
         resolve();
-      }, 100 * attempts * 10);
+      }, 100 + attempts * 10);
     });
   }
 }
@@ -139,7 +148,7 @@ async function hideOther(className) {
         }
         attempts = attempts + 1;
         resolve();
-      }, 100 * attempts * 10);
+      }, 100 + attempts * 10);
     });
   }
 }
@@ -159,7 +168,7 @@ async function showOther(className) {
         }
         attempts = attempts + 1;
         resolve();
-      }, 100 * attempts * 10);
+      }, 100 + attempts * 10);
     });
   }
 }
@@ -183,8 +192,6 @@ function blockByKeywords(res) {
   if(res['hide-commented-on']) keywords.push('commented on this')
   if(res['hide-by-companies']) keywords.push('href="https://www.linkedin.com/company/')
   if(res['hide-by-people']) keywords.push('href="https://www.linkedin.com/in/')
-
-  console.log(keywords)
 
   let posts;
 
@@ -234,39 +241,6 @@ window.mobileCheck = function() {
   (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
-
-function hideAll(res) {
-  if (res.master) {
-    //hideOther("account-center-container");
-    if (res.feed) {
-      hideFeed();
-      hideOther("feeds");
-      clearInterval(keywordInterval);
-    } else {
-      showFeed();
-      showOther("feeds");
-      blockByKeywords();
-    };
-    if (res.learning) {
-      hideOther("learning-top-courses");
-      hideOther("pv-course-recommendations")
-    } else {
-      showOther("learning-top-courses");
-      showOther("pv-course-recommendations")
-    };
-    if (res.ads) { hideOther("ad-banner-container"); } else { showOther("ad-banner-container"); }
-    //res.news ? hideOther("news-module") : showOther("news-module");
-  } else {
-    showFeed();
-    //showOther("feeds")
-    //showOther("account-center-container");
-    showOther("learning-top-courses");
-    showOther("ad-banner-container");
-    showOther("ads-container");
-    //showOther("news-module");
-    clearInterval(keywordInterval);
-  }
-}
 
 //Modified from https://gist.github.com/twhitacre/d4536183c22a2f5a8c7c427df04acc90
 async function selectMessagesForDeletion() {
