3b668da45fe0e1f7419706eeefb61a1642ae196f
njelich <12912633+njelich@users.noreply.github.com>
2022-03-13 18:36:03 +0100

    updated

--- content/content.js
+++ content/content.js
@@ -17,18 +17,18 @@ async function doIt(res) {
   if (res['main-toggle']) {
     //Feed
     if (res['hide-whole-feed']) {
-      hideFeed()
+      toggleFeed(true)
       hideOther('feeds')
       clearInterval(keywordInterval)
     } else {
-      showFeed()
+      toggleFeed(false)
       showOther('feeds')
       clearInterval(keywordInterval)
       blockByKeywords(res)
     }
   } else {
     //Feed
-    showFeed()
+    toggleFeed(false)
     showOther('feeds')
     clearInterval(keywordInterval)
   }
@@ -148,7 +148,7 @@ chrome.runtime.onMessage.addListener(function (request) {
 
 // Toggle feed
 
-async function hideFeed() {
+async function toggleFeed(shown) {
   let attempts = 0
   let success = false
 
@@ -161,34 +161,15 @@ async function hideFeed() {
           document.getElementsByClassName('artdeco-dropdown')[1]
             .nextElementSibling
         ) {
-          document
-            .getElementsByClassName('artdeco-dropdown')[1]
-            .nextElementSibling.classList.add('hide')
-          success = true
-        }
-        attempts = attempts + 1
-        resolve()
-      }, 100 + attempts * 10)
-    })
-  }
-}
-
-async function showFeed() {
-  let attempts = 0
-  let success = false
-
-  while (!success && attempts < 50) {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        if (
-          document.getElementsByClassName('artdeco-dropdown') &&
-          document.getElementsByClassName('artdeco-dropdown')[1] &&
-          document.getElementsByClassName('artdeco-dropdown')[1]
-            .nextElementSibling
-        ) {
-          document
-            .getElementsByClassName('artdeco-dropdown')[1]
-            .nextElementSibling.classList.remove('hide', 'dim')
+          if (shown) {
+            document
+              .getElementsByClassName('artdeco-dropdown')[1]
+              .nextElementSibling.classList.add('hide')
+          } else {
+            document
+              .getElementsByClassName('artdeco-dropdown')[1]
+              .nextElementSibling.classList.remove('hide', 'dim')
+          }
           success = true
         }
         attempts = attempts + 1
@@ -213,8 +194,10 @@ async function showOther(className) {
 // Block by keywords
 
 let keywordInterval
+let postCountPrompted = false
 
 function blockByKeywords(res) {
+  console.log('res', res)
   let keywords =
     res['feed-keywords'] == '' ? [] : res['feed-keywords'].split(',')
   if (res['hide-by-age'] !== 'disabled')
@@ -231,11 +214,7 @@ function blockByKeywords(res) {
   if (res['hide-promoted']) keywords.push('Promoted')
   if (res['hide-shared']) keywords.push('feed-shared-mini-update-v2')
   if (res['hide-followed']) keywords.push('following')
-  if (res['hide-liked'])
-    keywords.push(
-      'likes this',
-      'like this'
-    )
+  if (res['hide-liked']) keywords.push('likes this', 'like this')
   if (res['hide-other-reactions'])
     keywords.push(
       'loves this',
@@ -275,6 +254,13 @@ function blockByKeywords(res) {
             post.dataset.hidden = true
           }
         })
+      } else {
+        if (!postCountPrompted) {
+          postCountPrompted = true //Prompt only once when loading linkedin
+          alert(
+            'Scroll down to start blocking posts (LinkedIn needs at least 10 loaded to load new ones).\n\nTo disable this alert, toggle it under misc in LinkOff settings'
+          )
+        }
       }
     }, 100)
 }
@@ -323,6 +309,7 @@ window.mobileCheck = function () {
       /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
         a
       ) ||
+      // eslint-disable-next-line no-useless-escape
       /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(
         a.substr(0, 4)
       )
@@ -357,7 +344,7 @@ function enableDarkTheme() {
       return shift255(m)
     })
   style.innerHTML += addendum
-  console.log(style.innerHTML)
+
   document.body.appendChild(style)
 }
 
@@ -403,7 +390,7 @@ async function selectMessagesForDeletion() {
           container.scrollTop = scrollHeight
         }, 1000)
       } else {
-        alert('The page too long to load. Please try again.')
+        alert('The page took too long to load. Please try again.')
       }
     })
   }
